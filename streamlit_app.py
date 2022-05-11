ef9a64056abbe18b1050db467431dad1ff91e22e
mistry98 <100590777+mistry98@users.noreply.github.com>
2022-05-11 18:21:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,13 @@ streamlit.dataframe(fruits_to_show)
 
 
 # New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('WHat fruit would you like information about?')
-streamlit.write('The user entered', fruit_choice)
+#streamlit.header("Fruityvice Fruit Advice!")
+#fruit_choice = streamlit.text_input('WHat fruit would you like information about?')
+#streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
     
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
